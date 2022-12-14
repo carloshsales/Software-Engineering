01c907fcfd4e86ee8184280f6bba2ca4306e89b4
Carlos Henrique <carlosh.sales@outlook.com>
2022-12-13 22:49:12 -0300

    fix: condition is prime function

--- paradigms/procedures_and_functions/isPrime.py
+++ paradigms/procedures_and_functions/isPrime.py
@@ -1,6 +1,7 @@
 def isPrime(number: int) -> bool:
     count = 0
-    for key in range(1, number + 1):
+    if not(number < 2):
+        for key in range(1, number + 1):
         if number % key == 0:
             count += 1
         
