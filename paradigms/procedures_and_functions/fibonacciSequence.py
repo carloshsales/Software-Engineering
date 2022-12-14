9cd7d7bb4284b3b8e11490f6a2df35853dff1727
Carlos Henrique <carlosh.sales@outlook.com>
2022-12-13 23:13:24 -0300

    test: Docstring test

--- paradigms/procedures_and_functions/fibonacciSequence.py
+++ paradigms/procedures_and_functions/fibonacciSequence.py
@@ -1,6 +1,8 @@
+#Result the number in the Fibonacci sequence
 def fibonacciSequence(number):
     if number == 1 or number == 2:
         return 1
     else:
         return fibonacciSequence(number - 1) + fibonacciSequence(number - 2)
-    
+
+# print(help(fibonacciSequence))
