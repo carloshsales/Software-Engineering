9b4fdd5326ee6b3d500fde51c91c39f6e8d667a4
Carlos Henrique <carlosh.sales@outlook.com>
2022-12-13 22:50:02 -0300

    fix: function refactoring

--- paradigms/procedures_and_functions/factorialResult.py
+++ paradigms/procedures_and_functions/factorialResult.py
@@ -1,11 +1,19 @@
+# def factorialResult(number: int) -> int:
+#     result = 0
+
+#     if number == 0 or number == 1:
+#         return 1
+
+#     for indicie in range(1, number + 1):
+#         result += factorialResult(number - 1)
+
+#     return result
+
+
 def factorialResult(number: int) -> int:
     result = 0
 
     if number == 0 or number == 1:
         return 1
 
-    for indicie in range(1, number + 1):
-        result += factorialResult(number - 1)
-
-    return result
-    
+    return number * factorialResult(number - 1)
