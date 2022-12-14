9392829406555bdbb67ae117da6715728b1a4491
Carlos Henrique <carlosh.sales@outlook.com>
2022-12-13 21:34:55 -0300

    create: is prime function

---
+++ paradigms/procedures_and_functions/isPrime.py
@@ -0,0 +1,10 @@
+def isPrime(number: int) -> bool:
+    count = 0
+    for key in range(1, number + 1):
+        if number % key == 0:
+            count += 1
+        
+    if count == 2:
+        return True
+    
+    return False
